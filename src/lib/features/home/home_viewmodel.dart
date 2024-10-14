e980c82fe2fc8419df9637ced1d5f447d31590f8
akbar904 <zharfan@walturn.com>
2024-10-14 07:46:21 +0700

    Modify App: my-repo-40 - Success (#2)
    * Update files
    
    * Update files

--- src/lib/features/home/home_viewmodel.dart
+++ src/lib/features/home/home_viewmodel.dart
@@ -1,3 +1,4 @@
+
 import 'package:my_app_1/app/app.bottomsheets.dart';
 import 'package:my_app_1/app/app.dialogs.dart';
 import 'package:my_app_1/app/app.locator.dart';
@@ -5,31 +6,36 @@ import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class HomeViewModel extends BaseViewModel {
-  final _dialogService = locator<DialogService>();
-  final _bottomSheetService = locator<BottomSheetService>();
+	final _dialogService = locator<DialogService>();
+	final _bottomSheetService = locator<BottomSheetService>();
 
-  String get counterLabel => 'Counter is: $_counter';
+	List<Map<String, dynamic>> _sp500Metrics = [];
 
-  int _counter = 0;
+	List<Map<String, dynamic>> get sp500Metrics => _sp500Metrics;
 
-  void incrementCounter() {
-    _counter++;
-    rebuildUi();
-  }
+	Future<void> fetchSP500Metrics() async {
+		// Mock data for S&P 500 metrics
+		_sp500Metrics = [
+			{'name': 'AAPL', 'price': 150.55, 'change': 1.5},
+			{'name': 'GOOGL', 'price': 2800.12, 'change': -15.3},
+			{'name': 'AMZN', 'price': 3402.22, 'change': 10.1},
+		];
+		rebuildUi();
+	}
 
-  void showDialog() {
-    _dialogService.showCustomDialog(
-      variant: DialogType.infoAlert,
-      title: 'Steve Rocks!',
-      description: 'Give steve $_counter stars on Github',
-    );
-  }
+	void showDialog() {
+		_dialogService.showCustomDialog(
+			variant: DialogType.infoAlert,
+			title: 'S&P 500 Metrics',
+			description: 'Displaying mock data for S&P 500',
+		);
+	}
 
-  void showBottomSheet() {
-    _bottomSheetService.showCustomSheet(
-      variant: BottomSheetType.notice,
-      title: 'title',
-      description: 'desc',
-    );
-  }
+	void showBottomSheet() {
+		_bottomSheetService.showCustomSheet(
+			variant: BottomSheetType.notice,
+			title: 'S&P 500 Notice',
+			description: 'This is a notice for S&P 500 metrics',
+		);
+	}
 }
